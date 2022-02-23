d99e4f5e6491485e2d2866139349ced6fb2527f0
Dequisa <Dequisas@gmail.com>
2022-02-23 08:08:21 -0500

    added server to react app

---
+++ back-end/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+var corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "https://agitated-visvesvaraya-323d67.netlify.app/",
+  ],
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (request, response) => {
+  response.send("Welcome to the zoo database");
+});
+
+app.get("*", (request, response) => {
+  response.status(404).send("Sorry, Page Not Found");
+});
+module.exports = app;
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useState, useEffect } from "react";
+// import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [reply, setReply] = useState("");
+
+  useEffect(() => {
+    const accessServer = async () => {
+      try {
+        const res = await axios.get("http://localhost:3001");
+        setReply(res.data);
+      } catch (err) {
+        console.log("Error Returned from acessServer Request: ", err);
+      }
+    };
+    accessServer();
+  }, []);
+  return <h1>{reply}</h1>;
 }
 
 export default App;
