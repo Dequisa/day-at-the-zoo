7ae7b03cc4ff972a01c0f163058af4324bcd3fdc
Dequisa <Dequisas@gmail.com>
2022-02-24 18:26:47 -0500

    added redirect file

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,12 +1,12 @@
-DROP DATABASE IF EXISTS  zoo_animals;
+-- DROP DATABASE IF EXISTS  zoo_animals;
 
-CREATE DATABASE zoo_animals;
+-- CREATE DATABASE zoo_animals;
 
-\c zoo_animals;
+-- \c zoo_animals;
 
 CREATE TABLE animals(
     animals_id SERIAL PRIMARY KEY,
-    name TEXT,
+    name TEXT NOT NULL,
     location TEXT,
     img TEXT
 );
