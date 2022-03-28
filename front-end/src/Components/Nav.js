affed518abbd6f2ca57172e145e3a83f3c968d20
Dequisa <Dequisas@gmail.com>
2022-03-28 18:12:47 -0400

    modified nav

--- front-end/src/Components/Nav.js
+++ front-end/src/Components/Nav.js
@@ -7,21 +7,23 @@ import "./Nav.css";
 function Nav() {
   return (
     <div className="navbar">
-      <img src={logo} alt="bronx zoo icon" />
+      <div>
+        {" "}
+        <Link to={"/"}>
+          <img src={logo} alt="bronx zoo icon" />{" "}
+        </Link>
+      </div>
       <input
         className="search"
         value="Search for Zoo Animal"
         type="text"
         unselectable="on"
       ></input>
-      <button>
-        {" "}
-        <Link to={"/"}>Plan Your Visit </Link>
-      </button>
-      <button>
+
+      <div>
         {" "}
         <Link to={"/1"}>View Schedule </Link>
-      </button>
+      </div>
       <img src={plus2} style={{ width: "3%" }} alt="plus symbol" />
     </div>
   );
