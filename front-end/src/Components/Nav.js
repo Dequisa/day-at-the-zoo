6415648ef66b870aaf9ebdf278cfa189bb3fcb8f
Dequisa <Dequisas@gmail.com>
2022-03-28 16:27:29 -0400

    Nav madifications

--- front-end/src/Components/Nav.js
+++ front-end/src/Components/Nav.js
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom";
+import gorilla from "../assets/zoogorilla.jpeg";
+import logo from "../assets/logo.jpeg";
+import plus2 from "../assets/plus.jpeg";
+import "./Nav.css";
 
 function Nav() {
   return (
-    <div>
+    <div className="navbar">
+      <img src={logo} />
+      <input
+        className="search"
+        value="Search for Zoo Animal"
+        type="text"
+        unselectable="on"
+      ></input>
       <button>
         {" "}
         <Link to={"/"}>Plan Your Visit </Link>
@@ -11,6 +22,7 @@ function Nav() {
         {" "}
         <Link to={"/1"}>View Schedule </Link>
       </button>
+      <img src={plus2} style={{ width: "3%" }} />
     </div>
   );
 }
