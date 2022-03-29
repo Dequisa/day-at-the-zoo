da9411877a2431685e5c336a02997aeaaca51a99
Dequisa <Dequisas@gmail.com>
2022-03-29 14:50:40 -0400

    edits for netlify

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,7 @@
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import axios from "axios";
 
-// Pages
-import MainPage from "./Pages/MainPage";
-import SecondaryPage from "./Pages/SecondaryPage";
-import Nav from "./Navbar/Nav.js";
 import RenderPage from "./AnimalScheduler/Render.js";
 
 axios.defaults.withCredentials = true;
